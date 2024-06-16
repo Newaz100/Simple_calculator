ae16f7b0dd811d4d09f55f0ed6477a4443bddd05
Yeasin Newaz <145804835+Newaz100@users.noreply.github.com>
2024-06-16 16:18:32 +0600

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,42 @@
+namespace Lab1_windows_forms__app
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            double n1 = Convert.ToDouble(txtNumber1.Text);
+            double n2 = Convert.ToDouble(txtNumber2.Text);
+            double result = n1 + n2;
+            MessageBox.Show(n1 + "+" + n2 + " = " + result);
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            double n1 = Convert.ToDouble(txtNumber1.Text);
+            double n2 = Convert.ToDouble(txtNumber2.Text);
+            double result = n1 - n2;
+            MessageBox.Show(n1 + "-" + n2 + " = " + result);
+        }
+
+        private void btnMultiplication_Click(object sender, EventArgs e)
+        {
+            double n1 = Convert.ToDouble(txtNumber1.Text);
+            double n2 = Convert.ToDouble(txtNumber2.Text);
+            double result = n1 * n2;
+            MessageBox.Show(n1 + "*" + n2 + " = " + result);
+        }
+
+        private void btnDivision_Click(object sender, EventArgs e)
+        {
+            double n1 = Convert.ToDouble(txtNumber1.Text);
+            double n2 = Convert.ToDouble(txtNumber2.Text);
+            double result = n1 / n2;
+            MessageBox.Show(n1 + "/" + n2 + " = " + result);
+        }
+    }
+}
